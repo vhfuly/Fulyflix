15446e87aeb8882a04ffc5ca424cfee54e6ef838
Victor Fuly <victor_hugo_fuly@hotmail.com>
2020-07-27 19:43:53 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Menu from './components/menu'
+import dadosIniciais from './data/dados_iniciais.json'
+import BrannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div style={{background:"#141414"}}>
+      <Menu />
+      <BrannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription='O que faz uma desenvolvedora front-end? trabalhando na área.'
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+      <Carousel
+
+        category={dadosIniciais.categorias[1]}
+      />
+      <Carousel
+
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Carousel
+
+        category={dadosIniciais.categorias[3]}
+      />
+
+      <Carousel
+
+        category={dadosIniciais.categorias[4]}
+      />
+      <Carousel
+
+        category={dadosIniciais.categorias[5]}
+      />
+
+      <Footer/>
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
